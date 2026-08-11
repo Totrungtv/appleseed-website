4e50d04724e11775e4faee551fc21ae19c936718
Totrungtv <0898888269tv@gmail.com>
2026-08-12 02:12:06 +0700

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -2,7 +2,7 @@
 // Replace the value below with your Supabase Publishable/anon key.
 // NEVER put the service_role/secret key in a public website.
 
-const SUPABASE_URL = "https://https://nuismqcjyutqigdydfkg.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://nuismqcjyutqigdydfkg.supabase.co/rest/v1/";
 const SUPABASE_ANON_KEY = "sb_publishable_Z3pTEseMEeoiGU9dYJ1NwQ_Ko7li9Sz";
 
 const supabaseClient = window.supabase.createClient(
