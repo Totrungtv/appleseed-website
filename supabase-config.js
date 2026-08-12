d32a90a5ad489913d5b26c6705976a6bc055541a
Totrungtv <0898888269tv@gmail.com>
2026-08-12 11:59:07 +0700

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -4,7 +4,7 @@ window.SUPABASE_URL =
     "https://nuismqcjyutqigdydfkg.supabase.co";
 
 window.SUPABASE_ANON_KEY =
-    "sb_publishable_Z3pTEseMEeoiGU9dYJ1NwO_Ko7li9Sz";
+    "sb_publishable_Z3pTEseMEeoiGU9dYJ1NwQ_Ko7li9Sz";
 
 window.supabaseClient =
     window.supabase.createClient(
